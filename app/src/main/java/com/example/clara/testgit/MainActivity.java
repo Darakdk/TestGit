563c63ed155ec21592aaf5194b3db67c8893cc7e
ClaraRodriguez <clarirodri_83@hotmail.com>
2016-08-26 23:45:55 +0200

    Eso es para que veas lo guay que soy

--- app/src/main/java/com/example/clara/testgit/MainActivity.java
+++ app/src/main/java/com/example/clara/testgit/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.clara.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         //Hola mor estoy trabajando desde mi PC
 
         //Pero que mono es mor
+
+        Button btn = (Button) findViewById(R.id.btn);
+        final TextView texto = (TextView) findViewById(R.id.txtView);
+        final EditText edit = (EditText) findViewById(R.id.txtEdit);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText(edit.getText());
+            }
+        });
+
     }
+
+
 }
