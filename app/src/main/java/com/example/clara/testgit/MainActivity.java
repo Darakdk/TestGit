671980db1db6869944a9db4d18942d88a2199196
Darakdk <djvbvg@hotmail.com>
2016-09-04 20:01:33 +0200

    Layout

--- app/src/main/java/com/example/clara/testgit/MainActivity.java
+++ app/src/main/java/com/example/clara/testgit/MainActivity.java
@@ -14,21 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Hola mor estoy trabajando desde mi PC
-
-        //Pero que mono es mor
-
-        Button btn = (Button) findViewById(R.id.btn);
-        final TextView texto = (TextView) findViewById(R.id.txtView);
-        final EditText edit = (EditText) findViewById(R.id.txtEdit);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                texto.setText(edit.getText());
-            }
-        });
-
     }
 
 
