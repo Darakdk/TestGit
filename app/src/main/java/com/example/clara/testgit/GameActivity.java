10d86249f6d6dc2070d6246ba8ff1e2ebeb06ef0
Darakdk <djvbvg@hotmail.com>
2016-09-04 20:35:50 +0200

    Layout

--- app/src/main/java/com/example/clara/testgit/GameActivity.java
+++ app/src/main/java/com/example/clara/testgit/GameActivity.java
@@ -1,13 +1,46 @@
 package com.example.clara.testgit;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    private String zodiac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        zodiac="Aries";
+        final TextView txtSim = (TextView) findViewById(R.id.simbol);
+
+
+        final Handler h = new Handler();
+        final int delay = 3000; //milliseconds
+
+        h.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                Random rand = new Random();
+                int n = rand.nextInt(4);
+
+                switch (n)
+                {
+                    case 1: zodiac="Aries";break;
+                    case 2: zodiac="Tauro";break;
+                    case 3: zodiac="Piscis";break;
+                    default: zodiac="Cancer"; break;
+                }
+
+                txtSim.setText(zodiac);
+                h.postDelayed(this, delay);
+            }
+        }, delay);
     }
+
+
 }
