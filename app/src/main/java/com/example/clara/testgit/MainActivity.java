cac8d2335cce3efbcdcb5da211c8d0b1e1ea87ce
ClaraRodriguez <clarirodri_83@hotmail.com>
2016-09-04 20:17:49 +0200

    Eso es para que veas lo guay que soy

--- app/src/main/java/com/example/clara/testgit/MainActivity.java
+++ app/src/main/java/com/example/clara/testgit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clara.testgit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botonenter = (Button) findViewById(R.id.btnplay);
+        Button botonexit = (Button) findViewById(R.id.btnexit);
+
+        botonexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        botonenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
 
     }
 
